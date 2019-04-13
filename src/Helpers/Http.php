b5182d7fc11e64cfa11f5c19026ffb01f6644119
Andrey Helldar <helldar@ai-rus.com>
2019-04-13 17:05:33 +0300

    Added buildUrl() method

--- src/Helpers/Http.php
+++ src/Helpers/Http.php
@@ -33,4 +33,34 @@ class Http
     {
         return \parse_url($url, PHP_URL_HOST);
     }
+
+    /**
+     * Reverse function for parse_url() (http://php.net/manual/en/function.parse-url.php).
+     *
+     * @see https://gist.github.com/Ellrion/f51ba0d40ae1d62eeae44fd1adf7b704
+     *
+     * @param array $parsed_url
+     *
+     * @return string
+     */
+    public static function buildUrl(array $parsed_url)
+    {
+        $scheme = isset($parsed_url['scheme']) ? ($parsed_url['scheme'] . '://') : '';
+
+        $host = $parsed_url['host'] ?? '';
+        $port = isset($parsed_url['port']) ? (':' . $parsed_url['port']) : '';
+
+        $user = $parsed_url['user'] ?? '';
+
+        $pass = isset($parsed_url['pass']) ? (':' . $parsed_url['pass']) : '';
+        $pass = ($user || $pass) ? ($pass . '@') : '';
+
+        $path = $parsed_url['path'] ?? '';
+        $path = $path ? ('/' . ltrim($path, '/')) : '';
+
+        $query    = isset($parsed_url['query']) ? ('?' . $parsed_url['query']) : '';
+        $fragment = isset($parsed_url['fragment']) ? ('#' . $parsed_url['fragment']) : '';
+
+        return implode('', [$scheme, $user, $pass, $host, $port, $path, $query, $fragment]);
+    }
 }
