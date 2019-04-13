0f3e2cebb776d9116e7433a32e57398850257400
Andrey Helldar <helldar@ai-rus.com>
2019-04-13 17:00:17 +0300

    Added Dumper class

---
+++ src/Helpers/Dumper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Helldar\Support\Helpers;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class Dumper
+{
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @see https://gist.github.com/Ellrion/561fc48894a87b853917e0a5cec83181#file-helper-php
+     *
+     * @param $query
+     * @param bool $is_short
+     *
+     * @return array|string
+     */
+    public static function ddSql($query, bool $is_short = false, bool $is_return = false)
+    {
+        if ($query instanceof Builder) {
+            $query = $query->getQuery();
+        }
+
+        $sql = $query->toSql();
+
+        $bindings = array_map(function ($binding) {
+            return is_int($binding) || is_float($binding) ? $binding : "'{$binding}'";
+        }, $query->getBindings());
+
+        $raw      = vsprintf(str_replace(['%', '?'], ['%%', '%s'], $sql), $bindings);
+        $bindings = $query->getRawBindings();
+
+        if ($is_return) {
+            return $is_short ? $raw : compact('sql', 'bindings', 'raw');
+        }
+
+        dd($is_short ? $raw : compact('sql', 'bindings', 'raw'));
+    }
+}
