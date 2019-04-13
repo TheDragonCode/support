b008c3ff2bc1b4421c62ed2483046f41ca064916
Andrey Helldar <helldar@ai-rus.com>
2019-04-13 16:53:35 +0300

    Removed duplicated method

--- src/Helpers/Arr.php
+++ src/Helpers/Arr.php
@@ -5,29 +5,8 @@ namespace Helldar\Support\Helpers;
 class Arr
 {
     /**
-     * Returns the number of characters of the longest element in the array.
-     *
-     * @param array $array
-     *
-     * @return int
-     */
-    public static function itemValueMaxLength(array $array): int
-    {
-        $max = 0;
-
-        \array_map(function ($value) use (&$max) {
-            $value = \strlen((string) $value);
-
-            if ($value > $max) {
-                $max = $value;
-            }
-        }, $array);
-
-        return $max;
-    }
-
-    /**
-     * Renaming array keys. As the first parameter, a callback function is passed, which determines the actions for processing the value.
+     * Renaming array keys.
+     * As the first parameter, a callback function is passed, which determines the actions for processing the value.
      * The output of the function must be a string with a name.
      *
      * @param array $array
