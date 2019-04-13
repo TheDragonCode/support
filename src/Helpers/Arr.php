36a55e7371a30d18ec3811e76fb71751584f2b6a
Andrey Helldar <helldar@ai-rus.com>
2019-04-13 12:20:10 +0300

    Added Arr class

--- src/Helpers/Arr.php
+++ src/Helpers/Arr.php
@@ -25,4 +25,76 @@ class Arr
 
         return $max;
     }
+
+    /**
+     * Renaming array keys. As the first parameter, a callback function is passed, which determines the actions for processing the value.
+     * The output of the function must be a string with a name.
+     *
+     * @param array $array
+     * @param $callback
+     *
+     * @return array
+     */
+    public static function renameKeys(array $array, $callback): array
+    {
+        $result = [];
+
+        \array_map(function ($value, $key) use (&$result, $callback) {
+            $new          = $callback($key);
+            $result[$new] = $value;
+        }, \array_values($array), \array_keys($array));
+
+        return $result;
+    }
+
+    /**
+     * Get the size of the longest text element of the array.
+     *
+     * @param array $array
+     *
+     * @return int
+     */
+    public static function sizeOfMaxValue(array $array): int
+    {
+        return \mb_strlen(\max($array), 'UTF-8');
+    }
+
+    /**
+     * Push one a unique element onto the end of array.
+     *
+     * @param array $array
+     * @param array|mixed $values
+     *
+     * @return array
+     */
+    public static function addUnique(array $array, $values): array
+    {
+        if (\is_array($values) || \is_object($values)) {
+            \array_map(function ($value) use (&$array) {
+                $array = self::addUnique($array, $value);
+            }, $values);
+        } else {
+            \array_push($array, $values);
+        }
+
+        return \array_unique($array);
+    }
+
+    /**
+     * Sort an associative array in the order specified by an array of keys.
+     *
+     * @see https://gist.github.com/Ellrion/a3145621f936aa9416f4c04987533d8d#file-helper-php Original Source
+     *
+     * @param array $array
+     * @param array $sorter
+     *
+     * @return array
+     */
+    public static function sortByKeysArray(array $array, array $sorter)
+    {
+        $sorter = \array_intersect($sorter, \array_keys($array));
+        $array  = \array_merge(\array_flip($sorter), $array);
+
+        return $array;
+    }
 }
