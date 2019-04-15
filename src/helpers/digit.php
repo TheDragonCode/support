c00609d3bba17cf63482ae5a1001a187f4bab3a3
Andrey Helldar <helldar@ai-rus.com>
2019-04-15 12:35:59 +0300

    Added helpers

---
+++ src/helpers/digit.php
@@ -0,0 +1,33 @@
+<?php
+
+use Helldar\Support\Facades\Digit;
+
+if (!function_exists('factorial')) {
+    /**
+     * Calculating the factorial of a number.
+     *
+     * @param int $n
+     *
+     * @return float|int
+     */
+    function factorial(int $n = 0)
+    {
+        return Digit::factorial($n);
+    }
+}
+
+if (!function_exists('short_number')) {
+    /**
+     * Converts a number into a short version.
+     * eg: 1000 >> 1K
+     *
+     * @param float $number
+     * @param int $precision
+     *
+     * @return string
+     */
+    function short_number(float $number, int $precision = 1): string
+    {
+        return Digit::shortNumber($number, $precision);
+    }
+}
---
+++ src/helpers/strings.php
@@ -0,0 +1,62 @@
+<?php
+
+use Helldar\Support\Facades\Str;
+
+if (!function_exists('str_choice')) {
+    /**
+     * The str_choice function translates the given language line with inflection.
+     *
+     * @param float $number
+     * @param array $choice
+     * @param string $additional
+     *
+     * @return string
+     */
+    function str_choice(float $number, array $choice = [], string $additional = '')
+    {
+        return Str::choice($number, $choice, $additional);
+    }
+}
+
+if (!function_exists('e')) {
+    /**
+     * Escape HTML special characters in a string.
+     *
+     * @param string $value
+     * @param bool $double_encode
+     *
+     * @return string
+     */
+    function e(string $value, bool $double_encode = true): string
+    {
+        return Str::e($value, $double_encode);
+    }
+}
+
+if (!function_exists('de')) {
+    /**
+     * Convert special HTML entities back to characters.
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    function de(string $value)
+    {
+        return Str::de($value);
+    }
+}
+
+if (!function_exists('str_replace_spaces')) {
+    /**
+     * Replacing multiple spaces with a single space.
+     *
+     * @param string $value
+     *
+     * @return string|null
+     */
+    function str_replace_spaces(string $value): ?string
+    {
+        return Str::replaceSpaces($value);
+    }
+}
