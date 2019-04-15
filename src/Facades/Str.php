bee2915b4e3b00b0812c34324dc0bf151468c83d
Andrey Helldar <helldar@ai-rus.com>
2019-04-15 12:34:48 +0300

    Refactored parameter name

--- src/Facades/Str.php
+++ src/Facades/Str.php
@@ -44,19 +44,19 @@ class Str
     /**
      * The str_choice function translates the given language line with inflection.
      *
-     * @param string $value
+     * @param float $number
      * @param array $choice
      * @param string $additional
      *
      * @return string
      */
-    public static function choice(string $value, array $choice = [], string $additional = '')
+    public static function choice(float $number, array $choice = [], string $additional = '')
     {
         $result = $choice[0] ?? '';
-        $num    = (int) $value;
-        $mod    = $num % 10;
+        $number = (int) $number;
+        $mod    = $number % 10;
 
-        if ($mod == 0 || ($mod >= 5 && $mod <= 9) || ($num % 100 >= 11 && $num % 100 <= 20)) {
+        if ($mod == 0 || ($mod >= 5 && $mod <= 9) || ($number % 100 >= 11 && $number % 100 <= 20)) {
             $result = $choice[2] ?? '';
         } elseif ($mod >= 2 && $mod <= 4) {
             $result = $choice[1] ?? '';
