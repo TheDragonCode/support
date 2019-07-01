d6138e2fa8282298cfc91c181e63e291703c3be5
Andrey Helldar <helldar@ai-rus.com>
2019-07-01 12:12:49 +0300

    Added abstract uuid model for Laravel Eloquent

---
+++ src/Laravel/Eloquent/UuidModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Helldar\Support\Laravel\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+use Webpatser\Uuid\Uuid;
+
+abstract class UuidModel extends Model
+{
+    public $incrementing = false;
+
+    protected $primaryKey = 'uuid';
+
+    protected $keyType = 'string';
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            if (!($model->{$this->primaryKey} ?? false)) {
+                $model->{$this->primaryKey} = (string) Uuid::generate(4);
+            }
+        });
+    }
+
+    public function getRouteKeyName()
+    {
+        return $this->primaryKey;
+    }
+}
