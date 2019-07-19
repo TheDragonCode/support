02e442090d6b86ae658e741ca10e4e7cf264a127
Andrey Helldar <helldar@ai-rus.com>
2019-07-19 17:00:28 +0300

    Added the CompositeKeyModel class

---
+++ src/Laravel/Eloquent/CompositeKeysModel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Helldar\Support\Laravel\Eloquent;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class CompositeKeysModel extends Model
+{
+    public $incrementing = false;
+
+    /**
+     * Required!
+     *
+     * @var array
+     */
+    protected $primaryKey = [];
+
+    protected function setKeysForSaveQuery(Builder $query)
+    {
+        $keys = $this->primaryKey;
+
+        if (!\is_array($keys)) {
+            return $query->where($keys, $this->getAttribute($keys));
+        }
+
+        foreach ($keys as $key) {
+            $query->where($key, $this->getAttribute($key));
+        }
+
+        return $query;
+    }
+}
