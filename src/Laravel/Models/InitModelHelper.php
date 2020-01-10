851bdf590bb3a3e51915bc0dc2001ed04a2a07ec
Andrey Helldar <helldar@ai-rus.com>
2020-01-10 17:28:43 +0300

    Create InitModelHelper.php

---
+++ src/Laravel/Models/InitModelHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Helldar\Support\Laravel\Models;
+
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Container\BindingResolutionException;
+
+trait InitModelHelper
+{
+    /** @var \Helldar\Support\Laravel\Models\ModelHelper */
+    protected static $model_helper;
+
+    /**
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     * @return \Helldar\Support\Laravel\Models\ModelHelper
+     */
+    protected function model(): ModelHelper
+    {
+        if (static::$model_helper === null) {
+            static::$model_helper = Container::getInstance()
+                ->make(ModelHelper::class);
+        }
+
+        return static::$model_helper;
+    }
+}
