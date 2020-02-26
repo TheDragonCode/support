e3048319b7b2ba2bda0a4a81eb7ae061044e6390
Andrey Helldar <helldar@ai-rus.com>
2020-02-26 10:00:28 +0300

    Fixed Method hydrate not found.

--- src/Laravel/Models/InitModelHelper.php
+++ src/Laravel/Models/InitModelHelper.php
@@ -2,7 +2,6 @@
 
 namespace Helldar\Support\Laravel\Models;
 
-use Helldar\Support\Exceptions\MethodNotFoundException;
 use Illuminate\Container\Container;
 
 trait InitModelHelper
@@ -15,7 +14,7 @@ trait InitModelHelper
      *
      * @return \Helldar\Support\Laravel\Models\ModelHelper
      */
-    protected static function model(): ModelHelper
+    protected function model(): ModelHelper
     {
         if (static::$model_helper === null) {
             static::$model_helper = Container::getInstance()
@@ -23,16 +22,5 @@ trait InitModelHelper
         }
 
         return static::$model_helper;
-    }
-
-    public function __call($name, $arguments)
-    {
-        if (! \method_exists(self::class, $name)) {
-            throw new MethodNotFoundException(\class_basename(self::class), $name);
-        }
-
-        return $name === 'model'
-            ? static::model()
-            : $this->{$name}(...$arguments);
     }
 }
