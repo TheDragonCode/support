9c6e7b44356e948a1a044ed5f3c586dec46e41e6
Andrey Helldar <helldar@ai-rus.com>
2020-12-28 10:55:51 +0300

    Transfer the Directory class

---
+++ src/Helpers/Filesystem/Directory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Helldar\Support\Helpers\Filesystem;
+
+use DirectoryIterator;
+use Helldar\Support\Exceptions\DirectoryNotFoundException;
+
+final class Directory
+{
+    public function find(string $path): DirectoryIterator
+    {
+        if ($this->doesntExist($path)) {
+            throw new DirectoryNotFoundException($path);
+        }
+
+        return new DirectoryIterator($path);
+    }
+
+    public function names(string $path): array
+    {
+        $items = [];
+
+        foreach ($this->find($path) as $directory) {
+            if ($directory->isDir() && ! $directory->isDot()) {
+                $items[] = $directory->getFilename();
+            }
+        }
+
+        sort($items);
+
+        return array_values($items);
+    }
+
+    public function make(string $path, int $mode = 755): bool
+    {
+        if ($this->doesntExist($path)) {
+            return mkdir($path, $mode, true);
+        }
+
+        return true;
+    }
+
+    public function exists(string $path): bool
+    {
+        return file_exists($path) && is_dir($path);
+    }
+
+    public function doesntExist(string $path): bool
+    {
+        return ! $this->exists($path);
+    }
+}
