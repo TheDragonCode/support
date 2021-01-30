b555cf0453f4289bd1c361bc548f8d18ccd7f5e0
Andrey Helldar <helldar@ai-rus.com>
2021-01-30 20:46:10 +0300

    Update File.php

--- src/Facades/Helpers/Filesystem/File.php
+++ src/Facades/Helpers/Filesystem/File.php
@@ -11,7 +11,7 @@ use SplFileInfo;
  * @method static bool exists(string $path)
  * @method static bool isFile(DirectoryIterator|SplFileInfo|string $value)
  * @method static void delete(string|string[] $paths)
- * @method static void store(string $path, string $content, int $mode = 755)
+ * @method static void store(string $path, string $content, int $mode = 0755)
  * @method static void validate(DirectoryIterator|SplFileInfo|string $path)
  */
 final class File extends BaseFacade
