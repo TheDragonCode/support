1736a2e7556cd68f0137eb50a4695c09585c1043
Andrey Helldar <helldar@ai-rus.com>
2021-03-17 18:13:26 +0300

    Added `$callback` parameter to `Arr::ksort`

--- tests/Helpers/ArrTest.php
+++ tests/Helpers/ArrTest.php
@@ -468,23 +468,48 @@ final class ArrTest extends TestCase
             'add key' => 'Add key',
             'all key' => 'All key',
 
+            700 => 'Number 700',
+
+            '*' => 'asterisk',
+
             'q' => 1,
             'r' => 2,
             's' => 5,
             'w' => 123,
 
+            '-' => 'hyphen',
+
+            400 => 'Number 400',
+
             'all_key' => 'All_Key',
             'add_key' => 'Add_Key',
 
+            0   => 'Number 0',
+            '_' => 'underscore',
+
             'API key'      => 'API key',
             'Are you sure' => 'Are you sure',
+
+            'allkey' => 'AllKey',
+            'addkey' => 'AddKey',
         ];
 
         $target = [
+            '*' => 'asterisk',
+            '-' => 'hyphen',
+
+            0   => 'Number 0',
+            400 => 'Number 400',
+            700 => 'Number 700',
+
+            '_' => 'underscore',
+
             'add key'      => 'Add key',
             'add_key'      => 'Add_Key',
+            'addkey'       => 'AddKey',
             'all key'      => 'All key',
             'all_key'      => 'All_Key',
+            'allkey'       => 'AllKey',
             'API key'      => 'API key',
             'Are you sure' => 'Are you sure',
 
@@ -495,6 +520,87 @@ final class ArrTest extends TestCase
         ];
 
         $this->assertSame($target, $this->arr()->ksort($source));
+    }
+
+    public function testKsortCallback()
+    {
+        $source = [
+            'add key' => 'Add key',
+            'all key' => 'All key',
+
+            700 => 'Number 700',
+
+            '*' => 'asterisk',
+
+            'q' => 1,
+            'r' => 2,
+            's' => 5,
+            'w' => 123,
+
+            '-' => 'hyphen',
+
+            400 => 'Number 400',
+
+            'all_key' => 'All_Key',
+            'add_key' => 'Add_Key',
+
+            0   => 'Number 0',
+            '_' => 'underscore',
+
+            'API key'      => 'API key',
+            'Are you sure' => 'Are you sure',
+
+            'allkey' => 'AllKey',
+            'addkey' => 'AddKey',
+        ];
+
+        $target = [
+            '*' => 'asterisk',
+            '-' => 'hyphen',
+
+            '_' => 'underscore',
+
+            'add key'      => 'Add key',
+            'add_key'      => 'Add_Key',
+            'addkey'       => 'AddKey',
+            'all key'      => 'All key',
+            'all_key'      => 'All_Key',
+            'allkey'       => 'AllKey',
+            'API key'      => 'API key',
+            'Are you sure' => 'Are you sure',
+
+            'q' => 1,
+            'r' => 2,
+            's' => 5,
+            'w' => 123,
+
+            0   => 'Number 0',
+            400 => 'Number 400',
+            700 => 'Number 700',
+        ];
+
+        $callback = static function ($current, $next) {
+            $current = is_string($current) ? Str::lower($current) : $current;
+            $next    = is_string($next) ? Str::lower($next) : $next;
+
+            $specials = ['*', '-', '_'];
+
+            if ($current === $next) {
+                return 0;
+            }
+
+            if (in_array($current, $specials, true)) {
+                if (in_array($next, $specials, true)) {
+                    return $current < $next ? -1 : 1;
+                }
+
+                return -1;
+            }
+
+            return (is_string($current) && is_numeric($next)) || ($current < $next) ? -1 : 1;
+        };
+
+        $this->assertSame($target, $this->arr()->ksort($source, $callback));
     }
 
     public function testExists()
