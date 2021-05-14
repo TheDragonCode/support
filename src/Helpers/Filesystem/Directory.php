663f77aebbc25290470e2eba029836f0e4666a03
Andrey Helldar <helldar@ai-rus.com>
2021-05-14 12:51:39 +0300

    Small fix

--- src/Helpers/Filesystem/Directory.php
+++ src/Helpers/Filesystem/Directory.php
@@ -69,7 +69,7 @@ final class Directory
      */
     public function make(string $path, int $mode = 0755): bool
     {
-        return $this->doesntExist($path) ? mkdir($path, $mode, true) : true;
+        return ! $this->doesntExist($path) || mkdir($path, $mode, true);
     }
 
     /**
