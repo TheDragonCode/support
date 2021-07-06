735bd4088a8c421963088776011ceea47d9f2d3a
Andrey Helldar <andrey-helldar@users.noreply.github.com>
2021-07-06 13:06:53 +0000

    Apply fixes from StyleCI

--- tests/Facades/Helpers/Http/Builder/RemoveQueryMethodTest.php
+++ tests/Facades/Helpers/Http/Builder/RemoveQueryMethodTest.php
@@ -4,10 +4,10 @@ namespace Tests\Facades\Helpers\Http\Builder;
 
 use Helldar\Support\Facades\Http\Builder as BuilderFacade;
 use Helldar\Support\Helpers\Http\Builder;
-use Tests\Facades\Helpers\Http\Base;
-use TypeError;
-
 use const PHP_VERSION_ID;
+use Tests\Facades\Helpers\Http\Base;
+
+use TypeError;
 
 class RemoveQueryMethodTest extends Base
 {
