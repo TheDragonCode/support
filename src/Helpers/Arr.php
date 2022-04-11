abc1b8785316d10a798caf98f6fb911eeb3a93ad
GitHub Action <action@github.com>
2022-04-11 20:37:02 +0000

    Update code-style 💻

--- src/Helpers/Arr.php
+++ src/Helpers/Arr.php
@@ -804,7 +804,7 @@ class Arr
      *
      * @param string $path
      * @param string $stub
-     * @param \ArrayAccess|array $array
+     * @param ArrayAccess|array $array
      * @param callable $replace
      * @param bool $sort_keys
      */
