a370015d622e7821c6b75d574041dea5c5fdce4c
Andrey Helldar <helldar@dragon-code.pro>
2023-08-03 12:38:52 +0300

    Fixed code-style

--- src/Facades/Helpers/Str.php
+++ src/Facades/Helpers/Str.php
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @author Andrey Helldar <helldar@ai-rus.com>
+ * @author Andrey Helldar <helldar@dragon-code.pro>
  *
  * @copyright 2023 Andrey Helldar
  *
